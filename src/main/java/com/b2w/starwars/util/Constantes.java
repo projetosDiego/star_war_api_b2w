8ac85180831af84dbb784b464cf758c928c13058
Diego Rodrigues Gomes <diego.gomes@MJV.net>
2020-05-24 15:47:34 -0300

    make it better

--- src/main/java/com/b2w/starwars/util/Constantes.java
+++ src/main/java/com/b2w/starwars/util/Constantes.java
@@ -3,10 +3,10 @@ package com.b2w.starwars.util;
 public class Constantes {
 	
     public static final String URL_PESQUISA        = "https://swapi.dev/api/planets/?search=";
-    public static final String NOT_FOUND_ID        = "Id de planeta não encontrado na base.";
-    public static final String NOT_FOUND_NAME      = "Nome de planeta não encontrado na base.";
-    public static final String NOT_FOUND_PLANET    = "Planeta não encontrado na base.";
-    public static final String ALREADY_USED_OLANET = "Planeta já cadastrado na base.";
-    public static final String NO_PLANETS          = "Nnehum planeta cadastrado.";
+    public static final String NOT_FOUND_ID        = "Id de planeta não encontrado.";
+    public static final String NOT_FOUND_NAME      = "Nome de planeta não encontrado.";
+    public static final String NOT_FOUND_PLANET    = "Planeta não encontrado.";
+    public static final String ALREADY_USED_OLANET = "Planeta já cadastrado.";
+    public static final String NO_PLANETS          = "Nenhum planeta cadastrado.";
     
 }
